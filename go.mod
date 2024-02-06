ff17d1f1cf10ee98b0735b6b68c4e753b5f5ca6f
Fachrurozi <fachrurozi@misteraladin.com>
2024-02-06 12:23:37 +0700

    add test for use definition url

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/afdolriski/gosoap
+module github.com/fachrz/gosoap
 
 require (
 	golang.org/x/net v0.0.0-20190125091013-d26f9f9a57f3
--- soap_test.go
+++ soap_test.go
@@ -206,3 +206,46 @@ func TestProcess_doRequest(t *testing.T) {
 		t.Errorf("invalid WSDL")
 	}
 }
+
+func TestUseDefinitionURL(t *testing.T) {
+	soapAddresses := []*soapAddress{
+		{
+			Location: "http://demo.ilias.de/webservice/soap/server.php",
+		},
+	}
+	ports := []*wsdlPort{
+		{
+			SoapAddresses: soapAddresses,
+		},
+	}
+	service := []*wsdlService{
+		{
+			Ports: ports,
+		},
+	}
+	definition := &wsdlDefinitions{
+		Services: service,
+	}
+
+	client := &Client{
+		wsdl:             "https://demo.ilias.de:4330/webservice/soap/server.php?wsdl",
+		HttpClient:       &http.Client{},
+		UseDefinitionURL: true,
+		Definitions:      definition,
+	}
+
+	if client.getLocation() != "https://demo.ilias.de:4330/webservice/soap/server.php" {
+		t.Errorf("url invalid")
+	}
+
+	client2 := &Client{
+		wsdl:             "https://demo.ilias.de:4330/webservice/soap/server.php?wsdl",
+		HttpClient:       &http.Client{},
+		UseDefinitionURL: false,
+		Definitions:      definition,
+	}
+
+	if client2.getLocation() != "http://demo.ilias.de/webservice/soap/server.php" {
+		t.Errorf("url invalid")
+	}
+}
